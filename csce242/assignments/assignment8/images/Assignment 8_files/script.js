a72f516c1e8305a9a65ea65dfddeec9120bb2ffd
zakelguindi <89213113+zakelguindi@users.noreply.github.com>
2023-10-01 22:28:11 -0400

    animation works

--- csce242/assignments/assignment8/images/Assignment 8_files/script.js
+++
@@ -1,9 +0,0 @@
-
-
-const manbuttonclick = () => {
-  document.getElementById("runningmandiv").classList.add("hide");
-}
-
-window.onload = () => {
-  document.getElementById("manbutton").onclick = manbuttonclick; 
-}
